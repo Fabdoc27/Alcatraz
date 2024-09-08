a168945e570ebf5f89a7043e1fb86ffee79f2a5e
Ashraful <ashrafulkarim.dev@gmail.com>
2024-09-08 20:39:28 +0600

    search and filter feature added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,15 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $posts = Post::paginate(10);
+        $posts = Post::when($request->query('search'), function (Builder $query) use ($request) {
+            return $query->where('title', 'LIKE', '%'.$request->query('search').'%')
+                ->orWhere('content', 'LIKE', '%'.$request->query('search').'%');
+        })
+            ->when($request->query('category'), function (Builder $query) use ($request) {
+                return $query->whereRelation('category', 'slug', $request->query('category'));
+            })
+            ->latest()->paginate(10);
+
         $categories = Category::all();
 
         return view('blog.index', compact('posts', 'categories'));
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,8 +28,4 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
-
-    // public function getRouteKeyName() {
-    //     return 'slug';
-    // }
 }
